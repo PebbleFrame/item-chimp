da867392261ef029bdf1923ef7f2a8b97a28517c
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-29 17:02:36 -0500

    initial commit, scaffold app

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var browserify = require('browserify-middleware');
+var reactify = require('reactify');
+var nunjucks = require('nunjucks');
+var config = require('./client/config');
+
+var app = express();
+
+nunjucks.configure('server/templates/views', {
+  express: app
+});
+
+app.use(express.static('public'));
+
+app.get('/js/' + config.common.bundle, browserify(config.common.packages, {
+  cache: true,
+  precompile: true
+}));
+
+app.use('/js', browserify('./client/scripts', {
+  external: config.common.packages,
+  transform: ['reactify']
+}));
+
+app.get('*', function(req, res) {
+  res.render('index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Server listening on port ' + port);
+});
