5a98d3c41c63534918411b8ee5c2ab313edadea5
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-30 17:28:55 -0500

    (cleanup) remove fake walmart data

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,55 +1,5 @@
 React = require('react');
 
-var fakeData = [{
-  amazon: [{
-        "itemId": 25847978,
-        "parentItemId": 25847978,
-        "name": "Honey Nut Cheerios Cereal, 26.6 oz",
-        "salePrice": 4.58,
-        "upc": "016000459335",
-        "categoryPath": "Food/Breakfast & Cereal/Cereal",
-        "shortDescription": "This 26.6 oz Honey Nut Cheerios Cereal has the irresistible taste of golden honey, making it a family favorite. In addition, each little &quot;o&quot; contains soluble fiber from whole grain oats. As part of a heart-healthy eating plan, eating Honey Nut whole grain cereal each day may help lower your cholesterol. In fact, 3 grams of soluble fiber daily from whole grain oats (as provided by this cereal) with a diet low in saturated fat and cholesterol can reduce the risk of heart disease. Made with whole grains and low in fat, Cheerios also provides 12 essential vitamins and minerals. That's something that everyone can smile about. Tried and tested over three decades, this Honey Nut cereal is a tasty and nutritious day to start your day. It is also suitable for kosher diets.",
-        "longDescription": "&lt;br&gt;&lt;b&gt;Honey Nut Cheerios Cereal:&lt;/b&gt;&lt;ul&gt;&lt;li&gt;Whole grain oats are first ingredient&lt;/li&gt;&lt;li&gt;Fortified with 12 vitamins and minerals&lt;/li&gt;&lt;li&gt;Good source of calcium and excellent source of iron&lt;/li&gt;&lt;li&gt;Kosher Honey Nut cereal&lt;/li&gt;&lt;/ul&gt;",
-        "thumbnailImage": "http://i.walmartimages.com/i/p/00/01/60/00/45/0001600045933_100X100.jpg",
-        "productTrackingUrl": "http://linksynergy.walmart.com/fs-bin/click?id=|LSNID|&offerid=223073.7200&type=14&catid=8&subid=0&hid=7200&tmpid=1082&RD_PARM1=http%253A%252F%252Fwww.walmart.com%252Fip%252FHoney-Nut-Cheerios-Cereal-26.6-oz%252F25847978%253Faffp1%253DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%2526affilsrc%253Dapi",
-        "standardShipRate": 4.97,
-        "marketplace": false,
-        "modelNumber": "45933",
-        "productUrl": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Fwww.walmart.com%2Fip%2FHoney-Nut-Cheerios-Cereal-26.6-oz%2F25847978%3Faffp1%3DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi",
-        "customerRating": "4.737",
-        "numReviews": 57,
-        "customerRatingImage": "http://i2.walmartimages.com/i/CustRating/4_7.gif",
-        "categoryNode": "976759_976783_1001339",
-        "bundle": false,
-        "addToCartUrl": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Fwww.walmart.com%2Fcatalog%2Fselect_product.gsp%3Fproduct_id%3D25847978%26add_to_cart%3D1%26qty%3D1%26affp1%3DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi",
-        "affiliateAddToCartUrl": "http://linksynergy.walmart.com/fs-bin/click?id=|LSNID|&offerid=223073.7200&type=14&catid=8&subid=0&hid=7200&tmpid=1082&RD_PARM1=http%253A%252F%252Fwww.walmart.com%252Fcatalog%252Fselect_product.gsp%253Fproduct_id%253D25847978%2526add_to_cart%253D1%2526qty%253D1%2526affp1%253DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%2526affilsrc%253Dapi",
-        "availableOnline": true
-    }, {
-        "itemId": 25875664,
-        "parentItemId": 25875664,
-        "name": "Kellogg's Frosted Flakes Cereal, 33 oz",
-        "salePrice": 4.58,
-        "upc": "038000937729",
-        "categoryPath": "Food/Breakfast & Cereal/Cereal",
-        "shortDescription": "&lt;p&gt;Start the day off right with a bowl of Kellogg's Frosted Flakes Cereal. When paired with an 8-oz glass of milk, each serving offers a good source of Vitamin D, protein, potassium and calcium. This popular Kellogg's cereal can be part of a nutritious breakfast or enjoyed as a tasty snack.&lt;/p&gt;",
-        "longDescription": "&lt;p&gt;&lt;b&gt;Kellogg's Frosted Flakes Cereal:&lt;/b&gt;&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Good source of Vitamin D&lt;/li&gt;&lt;li&gt;They're gr-r-reat!&lt;/li&gt;&lt;li&gt;Kosher&lt;/li&gt;&lt;li&gt;Serve with milk or your favorite milk alternative&lt;/li&gt;&lt;li&gt;This Kellogg's cereal, 33 oz, is a good source of protein&lt;/li&gt;&lt;/ul&gt;",
-        "thumbnailImage": "http://i.walmartimages.com/i/p/00/03/80/00/93/0003800093772_100X100.jpg",
-        "productTrackingUrl": "http://linksynergy.walmart.com/fs-bin/click?id=|LSNID|&offerid=223073.7200&type=14&catid=8&subid=0&hid=7200&tmpid=1082&RD_PARM1=http%253A%252F%252Fwww.walmart.com%252Fip%252FKellogg-s-Frosted-Flakes-Cereal-33-oz%252F25875664%253Faffp1%253DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%2526affilsrc%253Dapi",
-        "standardShipRate": 4.97,
-        "marketplace": false,
-        "modelNumber": "3800093771",
-        "productUrl": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Fwww.walmart.com%2Fip%2FKellogg-s-Frosted-Flakes-Cereal-33-oz%2F25875664%3Faffp1%3DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi",
-        "customerRating": "4.463",
-        "numReviews": 41,
-        "customerRatingImage": "http://i2.walmartimages.com/i/CustRating/4_5.gif",
-        "categoryNode": "976759_976783_1001339",
-        "bundle": false,
-        "addToCartUrl": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Fwww.walmart.com%2Fcatalog%2Fselect_product.gsp%3Fproduct_id%3D25875664%26add_to_cart%3D1%26qty%3D1%26affp1%3DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi",
-        "affiliateAddToCartUrl": "http://linksynergy.walmart.com/fs-bin/click?id=|LSNID|&offerid=223073.7200&type=14&catid=8&subid=0&hid=7200&tmpid=1082&RD_PARM1=http%253A%252F%252Fwww.walmart.com%252Fcatalog%252Fselect_product.gsp%253Fproduct_id%253D25875664%2526add_to_cart%253D1%2526qty%253D1%2526affp1%253DQZgRg5FCruS1ZONZjdE96G2csJdrFkjK5r5qPrTCDm8%2526affilsrc%253Dapi",
-        "availableOnline": true
-    }]
-  }]
-
 // Centralized display for all components
 var DisplayBox = React.createClass({
   // Sets initial state properties to empty arrays to avoid undefined errors
