60cc7f91d59987213d91b1ee7d10fbb6bd8431ef
Vini <gvinaya@hotmail.com>
2015-04-30 13:53:39 -0700

    adding walmart general query response

--- server.js
+++ server.js
@@ -26,13 +26,26 @@ app.get('*', function(req, res) {
   res.render('index.html');
 });
 
-app.post('/general-query', function(req, res) {
+app.post('/general-query-amazon', function(req, res) {
   res.send([
-    {amazon: [{amazon1: 'amazon1'}, {amazon2: 'amazon2'}]},
-    {walmart: [{walmart1: 'walmart1'}, {walmart2: 'walmart2'}]},
-    {bestBuy: [{bestBuy1: 'bestBuy1'}, {bestBuy2: 'bestBuy2'}]}
+    {amazon: [{amazon1: 'amazon1'}, {amazon2: 'amazon2'}]}
   ]);
 });
+
+app.post('/general-query-walmart', function(req, res) {
+  res.send([
+    {walmart: [{"itemId":30135922 ,"parentItemId":30135922,"name":"Apple iPod touch 32GB","msrp":249,"salePrice":229,"upc":"885909827367","categoryPath":"Electronics/iPods & MP3 Players/Apple iPods"},
+    {"itemId":21805444,"parentItemId":21805444,"name":"Apple iPod nano 16GB","msrp":145,"salePrice":145,"upc":"885909564910","categoryPath":"Electronics/iPods & MP3 Players/All MP3 Players"}]}
+  ]);
+});
+
+app.post('/general-query-bestbuy', function(req, res) {
+  res.send([
+    {bestbuy: [{bestbuy1: 'bestbuy1'}, {bestbuy2: 'bestbuy2'}]}
+  ]);
+});
+
+
 
 var port = process.env.PORT || 3000;
 
