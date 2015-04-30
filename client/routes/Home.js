7f77b149fa7e917873314c9eeed6260003f475ca
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-30 17:59:17 -0500

    (cleanup) move Walmart components to external file

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var WalmartComponents = require('./Home-Walmart-Components');
+var WalmartRelatedResultsDisplay = WalmartComponents.WalmartRelatedResultsDisplay;
+var WalmartIndividualResultDisplay = WalmartComponents.WalmartIndividualResultDisplay;
 
 // Centralized display for all components
 var DisplayBox = React.createClass({
@@ -104,37 +107,6 @@ var AmazonIndividualResultDisplay = React.createClass({
   render: function() {
     return (
       <div className="amazon-individual-display">
-        <h4 className="product-name">
-          {this.props.name}
-        </h4>
-      </div>
-    );
-  }
-});
-
-// Component that displays related results from Walmart API
-var WalmartRelatedResultsDisplay = React.createClass({
-  render: function() {
-    var resultNodes = this.props.data.walmart.map(function(result, index) {
-      console.log(result.name);
-      return (
-        <WalmartIndividualResultDisplay name={result.name} />
-      );
-    });
-    return (
-      <div className="related-results-display hidden">
-        <h3>Walmart Related Results</h3>
-        {resultNodes}
-      </div>
-    );
-  }
-});
-
-// Component that displays individual results for Walmart
-var WalmartIndividualResultDisplay = React.createClass({
-  render: function() {
-    return (
-      <div className="walmart-individual-display">
         <h4 className="product-name">
           {this.props.name}
         </h4>
