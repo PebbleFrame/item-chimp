a9f4caf55697332ad31ca2c935d9e1dd2f0c2b8b
Jeff Peoples <meisterpeeps@gmail.com>
2015-05-01 12:50:33 -0700

    checks if user exists in database on login

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,19 @@
 var Bookshelf = require('bookshelf');
+var events = require('events');
+var EventEmitter = require("events").EventEmitter;
+var util = require('util');
+var Q    = require('q');
+var jwt  = require('jwt-simple');
+
 
 //Create db wrapper for database to 
-var db = {}
+function DB(){
+  EventEmitter.call(this);
+};
 
+util.inherits(DB, EventEmitter);
+
+var db = new DB();
 
 var knex = require('knex')({
   client: 'mysql',
@@ -80,7 +91,7 @@ db.orm = require('bookshelf')(knex);
 //-------------ORM FOR USERS END-------------------/
 
 //-------------ORM FOR REVIEWS START---------------/
-  //Create user Model
+  //Create Review Model
   db.Review = db.orm.Model.extend({
      tableName:"reviews"
   });
@@ -175,10 +186,31 @@ db.orm = require('bookshelf')(knex);
 //-------------ORM FOR WATCHERS END----------------/
 
 //-------------API CONFIGURATION START-------------/
-db.insert = function(model,obj){
-  var obj = new this.[model](obj);
+// db.insert = function(model,obj){
+//   var obj = new this.[model](obj);
 
-}
+// }
+  db.findUser = function(userName){
+    db.User.findOne({username: userName})
+    .then(function (user) {
+      if (!user) {
+        next(new Error('User does not exist'));
+      } else {
+        return user.comparePasswords(password)
+          .then(function(foundUser) {
+            if (foundUser) {
+              var token = jwt.encode(user, 'secret');
+              res.json({token: token});
+            } else {
+              return next(new Error('No user'));
+            }
+          });
+      }
+    })
+    .fail(function (error) {
+      next(error);
+    });
+  };
 
 //-------------API CONFIGURATION START-------------/
 
