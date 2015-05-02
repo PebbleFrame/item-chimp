b5a68833123fc03ca509908f194fd905da8adba5
Christina Holland <hsubox@gmail.com>
2015-05-01 21:25:15 -0700

    D3 chart code moved into React component w/o changing functionality.

--- client/routes/D3-Chart.js
+++ client/routes/D3-Chart.js
@@ -1,6 +1,12 @@
 var React = require('react');
+var d3Engine = require('../../public/d3_reviews.js')
 
 var D3Chart = React.createClass({
+  componentDidMount: function() {
+    var el = this.getDOMNode();
+    console.log(this.getDOMNode());
+    d3Engine.create(el);
+  },
   render: function() {
     return (
       <div className="d3-container">
--- public/d3_reviews.js
+++ public/d3_reviews.js
@@ -31,186 +31,200 @@ var dataGen = function () {
 
 // ------ CONFIG DATA ---------
 
-var data = dataGen();
+var d3Engine = {};
 
-var legendData = ['0 stars', '1 star', '3 stars', '4 stars', '5 stars'];
+
+d3Engine.data = dataGen();
+
+d3Engine.legendData = ['0 stars', '1 star', '3 stars', '4 stars', '5 stars'];
 
 // chart vars
-var width = 600;
-var height = 300;
+d3Engine.width = 600;
+d3Engine.height = 300;
 
 // tooltip vars
-var ttOffset = 10;
-var ttWidth = 220;
-var ttHeight = 105;
+d3Engine.ttOffset = 10;
+d3Engine.ttWidth = 220;
+d3Engine.ttHeight = 105;
 
 // x based on star rating
-var x = d3.scale.linear()
+d3Engine.x = d3.scale.linear()
           .domain([0, 6])
-          .range([0, width]);
+          .range([0, d3Engine.width]);
 
 // narrower range for foci (don't want foci at edge)
-var fociX = d3.scale.linear()
+d3Engine.fociX = d3.scale.linear()
           .domain([0, 6])
-          .range([100, width-50]);
+          .range([100, d3Engine.width-50]);
 
 var fociGen = function (numFoci, x) {
   var results = [];
   for (var i = 0; i < numFoci; i++) {
-    results.push({x: fociX(i+1)/2, y: 150});
+    results.push({x: d3Engine.fociX(i+1)/2, y: 150});
   }
   return results;
 };
 
-var foci = fociGen(11, x);
+d3Engine.foci = fociGen(11, d3Engine.x);
 
 // ---------------------------------
 
 
 // ------ CHART COMPONENTS ---------
 
-var chart = d3.select(".chart")
-  .attr("width", width)
-  .attr("height", height);
+d3Engine.create = function (el, props, state) {
+  this.chart = d3.select(".chart")
+    .attr("width", d3Engine.width)
+    .attr("height", d3Engine.height);
 
-var circle = chart.selectAll("g.node")
-    .data(data)
-  .enter().append("g")
-    .classed("node", true)
-    .attr("transform", function(d, i) { 
-      return "translate(" + (x(d.stars)+ d.numLines) + ", 50)";
-    });
+  var circle = this.chart.selectAll("g.node")
+      .data(d3Engine.data)
+    .enter().append("g")
+      .classed("node", true)
+      .attr("transform", function(d, i) { 
+        return "translate(" + (d3Engine.x(d.stars)+ d.numLines) + ", 50)";
+      });
 
-circle.append("circle")
-  .attr("cx", 0)
-  .attr("cy", 0)
-  .attr("r", function(d) { return d.numLines/2; })
-  .style("fill", function(d) { return d.site.color; })
-  .style("stroke", "white")
-  .style("stroke-width", 2)
-  .style("stroke-opacity", 0.5);
+  circle.append("circle")
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .attr("r", function(d) { return d.numLines/2; })
+    .style("fill", function(d) { return d.site.color; })
+    .style("stroke", "white")
+    .style("stroke-width", 2)
+    .style("stroke-opacity", 0.5);
 
-circle.append("text")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("dy", ".35em")
-  .text(function(d) {return d.stars;});
+  circle.append("text")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("dy", ".35em")
+    .text(function(d) {return d.stars;});
 
-var legend = chart.selectAll("g.legend")
-  .data(legendData)
-  .enter().append("g")
-  .classed("legend", true)
-  .attr("transform", "translate(0, " + (height-25) + ")");
+  var legend = this.chart.selectAll("g.legend")
+    .data(d3Engine.legendData)
+    .enter().append("g")
+    .classed("legend", true)
+    .attr("transform", "translate(0, " + (d3Engine.height-25) + ")");
 
-legend.append("text")
-  .attr("x", function(d, i) { return x((i*1.25)+0.3); })
-  .attr("y", 0)
-  .text(function(d) {return d});
+  legend.append("text")
+    .attr("x", function(d, i) { return d3Engine.x((i*1.25)+0.3); })
+    .attr("y", 0)
+    .text(function(d) {return d});
 
-var storeLegend = chart.selectAll("g.storeLegend")
-  .data(storeData)
-  .enter().append("g")
-  .classed("storeLegend", true)
-  .attr("transform", "translate(20,10)");
+  var storeLegend = this.chart.selectAll("g.storeLegend")
+    .data(storeData)
+    .enter().append("g")
+    .classed("storeLegend", true)
+    .attr("transform", "translate(20,10)");
 
-storeLegend.append("rect")
-  .attr("x", function (d,i) { return i*100; })
-  .attr("y", 5)
-  .attr("width", 80)
-  .attr("height", 25)
-  .style("fill", function (d) { return d.color; });
+  storeLegend.append("rect")
+    .attr("x", function (d,i) { return i*100; })
+    .attr("y", 5)
+    .attr("width", 80)
+    .attr("height", 25)
+    .style("fill", function (d) { return d.color; });
 
-storeLegend.append("text")
-  .attr("x", function (d,i) { return i*100 + 5; })
-  .attr("y", 18)
-  .attr("dy", "0.35em")
-  .text(function(d) { return d.name; } );
+  storeLegend.append("text")
+    .attr("x", function (d,i) { return i*100 + 5; })
+    .attr("y", 18)
+    .attr("dy", "0.35em")
+    .text(function(d) { return d.name; } );
+
+  tooltipSetup();
+  forceInit();
+};
+
+//d3Engine.create();
+
 
 // ---------------------------------
 
 
 // ------ TOOLTIP DEF ---------
 
+function tooltipSetup() {
+  tooltip = d3.select(".d3-container")
+    .append("div")
+    .style("width", d3Engine.ttWidth + "px")
+    .style("height", d3Engine.ttHeight + "px")
+    .classed("hoverbox", true);
 
-var tooltip = d3.select(".d3-container")
-  .append("div")
-  .style("width", ttWidth + "px")
-  .style("height", ttHeight + "px")
-  .classed("hoverbox", true);
+  tooltip.append('div')
+    .classed("username", true);
 
-tooltip.append('div')
-  .classed("username", true);
+  tooltip.append('div')
+    .classed("reviewTitle", true);
 
-tooltip.append('div')
-  .classed("reviewTitle", true);
+  tooltip.append('div')
+    .classed("reviewText", true);
 
-tooltip.append('div')
-  .classed("reviewText", true);
+  var nodes = d3Engine.chart.selectAll("g.node");
 
-// ---------------------------------
+  nodes.on('mouseover', function(d) {
+    var mouseLoc = d3.mouse(this.parentNode);
+    if (mouseLoc[0] + d3Engine.ttOffset + d3Engine.ttWidth > d3Engine.width) {
+      mouseLoc[0] = mouseLoc[0] - d3Engine.ttOffset*2 - d3Engine.ttWidth;
+    }
+    if (mouseLoc[1] + d3Engine.ttOffset + d3Engine.ttHeight > d3Engine.height) {
+      mouseLoc[1] = mouseLoc[1] - d3Engine.ttOffset*2 - d3Engine.ttHeight;
+    }
+    tooltip
+          .style("display", "block")
+          .style("left", (mouseLoc[0]+d3Engine.ttOffset)+"px")
+          .style("top", (mouseLoc[1]+d3Engine.ttOffset)+"px")
+          .transition()
+          .duration(200)
+          .style('opacity', 1);
+    tooltip.select(".username")
+      .text(d.username + " on " + d.site.name);
+    tooltip.select(".reviewTitle")
+      .text(d.reviewTitle);
+    tooltip.select(".reviewText")
+      .text(d.reviewStart);
+  });
 
+  nodes.on('mouseout', function(d) {
+    tooltip.transition()
+      .duration(200)
+      .style('opacity', 0)
+      .each('end', function () {
+        tooltip.style("display", "none");
+      });
+  });
+}
 
-// ------ TOOLTIP HOVER BEHAVIOR ---------
-
-var nodes = chart.selectAll("g.node");
-
-nodes.on('mouseover', function(d) {
-  var mouseLoc = d3.mouse(this.parentNode);
-  if (mouseLoc[0] + ttOffset + ttWidth > width) {
-    mouseLoc[0] = mouseLoc[0] - ttOffset*2 - ttWidth;
-  }
-  if (mouseLoc[1] + ttOffset + ttHeight > height) {
-    mouseLoc[1] = mouseLoc[1] - ttOffset*2 - ttHeight;
-  }
-   tooltip
-        .style("display", "block")
-        .style("left", (mouseLoc[0]+ttOffset)+"px")
-        .style("top", (mouseLoc[1]+ttOffset)+"px")
-        .transition()
-        .duration(200)
-        .style('opacity', 1);
-  tooltip.select(".username")
-    .text(d.username + " on " + d.site.name);
-  tooltip.select(".reviewTitle")
-    .text(d.reviewTitle);
-  tooltip.select(".reviewText")
-    .text(d.reviewStart);
-});
-
-nodes.on('mouseout', function(d) {
-  tooltip.transition()
-    .duration(200)
-    .style('opacity', 0)
-    .each('end', function () {
-      tooltip.style("display", "none");
-    });
-});
-
-// ---------------------------------
+// // ---------------------------------
 
 
 // ------ FORCE DEFINITION AND START---------
 
-var force = d3.layout.force()
-  .gravity(0)
-  .links([])
-  .nodes(data)
-  .charge(function(d) { return d.numLines * -1.5; })
-  .size([width, height]);
+function forceInit() {
+  var force = d3.layout.force()
+    .gravity(0)
+    .links([])
+    .nodes(d3Engine.data)
+    .charge(function(d) { return d.numLines * -1.5; })
+    .size([d3Engine.width, d3Engine.height]);
 
-force.start();
+  force.start();
 
-force.on("tick", function(e) {
-  var k = .1 * e.alpha;
+  force.on("tick", function(e) {
+    var k = .1 * e.alpha;
 
-  data.forEach(function(o,i) {
-    o.y += (foci[o.stars*2].y - o.y) * k;
-    o.x += (foci[o.stars*2].x - o.x) * k;
-    chart.selectAll("g.node")
-      .attr("transform", function(d) { 
-        return "translate(" + d.x + "," + d.y + ")";
-      });
+    d3Engine.data.forEach(function(o,i) {
+      o.y += (d3Engine.foci[o.stars*2].y - o.y) * k;
+      o.x += (d3Engine.foci[o.stars*2].x - o.x) * k;
+      d3Engine.chart.selectAll("g.node")
+        .attr("transform", function(d) { 
+          return "translate(" + d.x + "," + d.y + ")";
+        });
+    });
   });
-});
+}
 
 // ---------------------------------
+
+
+module.exports = d3Engine;
+
+
