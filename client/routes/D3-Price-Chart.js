22305f91629dbfccfc0258885564ed36a74d2357
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-04 21:03:52 -0500

    (feat) add to D3 price chart

--- client/routes/D3-Price-Chart.js
+++ client/routes/D3-Price-Chart.js
@@ -4,28 +4,34 @@ var d3PriceEngine = require('../scripts/d3PriceEngine');
 module.exports = React.createClass({
   startEngine: function(width, height) {
 
-    var el = this.getDOMNode();
-
-    var prices = [];
+    var query = this.props.query;
+    var pricesArray = [];
 
     this.props.walmartRelatedResults.walmart.forEach(function(item) {
-      var itemOrganized = {
-        name: item.name,
-        price: item.salePrice,
+      var itemObject = {
+        salePrice: item.salePrice,
         source: 'Walmart'
       };
+      pricesArray.push(itemObject);
     });
 
-    d3PriceEngine.create(el, width, height, prices);
+    this.props.bestbuyRelatedResults.bestbuy.forEach(function(item) {
+      var itemObject = {
+        salePrice: item.salePrice,
+        source: 'Best Buy'
+      };
+      pricesArray.push(itemObject);
+    });
+
+
+    d3PriceEngine(pricesArray, query);
 
   },
 
   render: function() {
     return (
       <div className="d3-price-container">
-        <hr />
-        <svg className="price-chart"></svg>
-        <hr />
+
       </div>
     );
   }
