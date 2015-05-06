5d85f6f5b27a7f253ffe48c56c80cfbffa23d41d
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-05 20:28:19 -0500

    (doc) add comments to D3-Price-Chart.js

--- client/routes/D3-Price-Chart.js
+++ client/routes/D3-Price-Chart.js
@@ -4,9 +4,12 @@ var d3PriceEngine = require('../scripts/d3PriceEngine');
 module.exports = React.createClass({
   startEngine: function(width, height) {
 
+    // Query will be displayed at the top of the D3 price chart
     var query = this.props.query;
+    // This array will populate the D3 data
     var pricesArray = [];
 
+    // Push the 10 walmart results (name and price) to pricesArray
     this.props.walmartRelatedResults.results.forEach(function(item) {
       var itemObject = {
         name: item.name,
@@ -16,6 +19,8 @@ module.exports = React.createClass({
       pricesArray.push(itemObject);
     });
 
+    // Push the 10 best buy results (name and price) to pricesArray
+    // Number of results for each store must be the same for the way the D3 price chart is currently set up
     this.props.bestbuyRelatedResults.results.forEach(function(item) {
       var itemObject = {
         name: item.name,
@@ -25,7 +30,7 @@ module.exports = React.createClass({
       pricesArray.push(itemObject);
     });
 
-
+    // Create the D3 price chart
     d3PriceEngine(pricesArray, query);
 
   },
@@ -33,10 +38,13 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="d3-price-container">
+      
         <div className="d3-price-chart-query">
         Results for <strong>{this.props.query}</strong>
         </div>
+
         <svg className="price-chart"></svg>
+
       </div>
     );
   }
